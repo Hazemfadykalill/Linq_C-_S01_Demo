126b1d43514068053f74b3ad637c235c5edd36b3
Hazem Fady <hazemfady123@gmail.com>
2024-08-11 15:33:56 +0300

    Add project files.

---
+++ Linq_C#_S01_Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Linq_C__S01_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
