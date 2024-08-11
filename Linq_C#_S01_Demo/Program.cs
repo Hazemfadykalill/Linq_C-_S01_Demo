533330fc9359b480a9433c26962e328a648921b4
Hazem Fady <hazemfady123@gmail.com>
2024-08-11 16:25:51 +0300

    C# Keywords [ var - dynamic ]

--- Linq_C#_S01_Demo/Program.cs
+++ Linq_C#_S01_Demo/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region C# Keywords [var - Dynamic]
+            #region Var
+            //var x;//invalid
+            //var Y = "Date";
+            //    Y = 30;//invalid
+            ////var Not use in parameter function and not use return type
+            ////var At Compilation type
+            ////var Not A data Type 
+            //// Must be initialize value to detect data type
+
+            #endregion
+            #region Dynamic
+
+            //dynamic
+            //at run time
+            //dynamic data = "haze";
+            //data = true;//invalid
+            //data = null;
+            //dynamic x;//invalid
+            //dynamic like var in JavaScript
+            //dynamic in run time like object at compile time
+            //dynamic can use (parameter ,return type) with function 
+            #endregion
+
+            #endregion
         }
     }
 }
