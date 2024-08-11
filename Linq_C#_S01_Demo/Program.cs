8f593468480f44c1257489e85e6e9fa09e50c2e9
Hazem Fady <hazemfady123@gmail.com>
2024-08-11 19:37:06 +0300

    Query Syntax

--- Linq_C#_S01_Demo/Program.cs
+++ Linq_C#_S01_Demo/Program.cs
@@ -55,6 +55,61 @@ namespace Linq_C__S01_Demo
             //}
 
             #endregion
+
+            #region LinQ
+
+            #region Overview
+            // List<int> list = new List<int>() { 1,2,3,4,5,6,7,8,10,12};
+            //var Result= list.Where(x => x % 2 == 0);
+            // foreach (var x in Result)
+            // { 
+            //     Console.Write(" " + x);
+            // } 
+            #endregion
+            #region Syntax LINQ
+            #region 1. Fluent LINQ
+            //use linQ method
+            //1.1 use linQ operator throw class Name [Enumerable]
+            //List<int> Numbers=new List<int>() { 1,2,3,4,10,5,6,7,8,9};  
+            //var Result=Enumerable.Where(Numbers,item =>item%2==0);
+            //foreach (var item in Result)
+            //{
+            //    Console.Write(" " +  item); 
+
+            //}
+
+
+            //1.2 use linQ operator throw Extension Method [Recommended]
+            //List<int> Numbers = new List<int>() { 1, 2, 3, 4, 10, 5, 6, 7, 8, 9 };
+            //var Result =Numbers.Where( item => item % 2 == 0);
+            //foreach (var item in Result)
+            //{
+            //    Console.Write(" " + item);
+
+            //}
+            #endregion
+
+            #region 2. Query syntax
+            //call too query expression
+            //like SQL  style 
+            //start always c# keyword from 
+            //end  always c# keyword select
+
+
+            //List<int> Numbers = new List<int>() { 1,2,3,4,5,6,4,5,8,9,7,10,15,17};
+            //var Result=from N in Numbers
+            //           where N%2==0
+            //           select N;
+            //foreach (var item in Result)
+            //{
+            //    Console.Write(" " + item);
+            //}
+
+            #endregion
+            #endregion
+
+
+            #endregion
         }
     }
 }
